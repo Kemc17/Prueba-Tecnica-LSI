737bffc54a62abf995c1d9cfec36aa7eaab8018c
Kemc17 <00129421@uca.edu.sv>
2025-11-14 20:37:00 -0600

    add: adding automated test examples with playwright

---
+++ pages/LoginPage.ts
@@ -0,0 +1,26 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly usernameInput; 
+  readonly passwordInput; 
+  readonly loginButton;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.locator('#username');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('#login-button');
+  }
+
+  async goto() {
+    await this.page.goto('https://www.saucedemo.com/v1/index.html');
+  }
+
+  async login(usarname: string, password: string) {
+    await this.usernameInput.fill(usarname);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click(); 
+  }
+
+}
